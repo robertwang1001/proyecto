6791454ccbdd9b233ecdf1b5df1d8bff9f31b84c
Robert Wang <glorywong1001@gmail.com>
2026-05-25 03:38:22 +0800

    chore: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,25 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   rules: {
-    'no-console': 'off',
+    'unused-imports/no-unused-imports': 'warn',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
+  typescript: {
+    overrides: {
+      'ts/no-unused-expressions': ['error', {
+        allowShortCircuit: true,
+        allowTernary: true,
+        allowTaggedTemplates: true,
+      }],
+    },
   },
 }, {
   ignores: ['bun.lock'],
